71bb34c8ba4dbd0487d13c6b76ca45d6772571c2
Diogenes Borges <diogenes.borges@email.com>
2025-07-02 16:20:52 -0300

    Atualizacao da pagina Medidas

--- src/Paginas/Medidas.js
+++ src/Paginas/Medidas.js
@@ -3,7 +3,17 @@ import React from "react";
 const Medidas = ()=>{
     return (
         <div>
-            <h1>Medidas</h1>
+            <div className="card">
+                <image src = ''/>
+                <div>
+                    <h3>Device</h3>
+                    <h4>Kw/h :</h4>
+                    <h4>Corrente :</h4>
+                    <h4>Voltagem :</h4>
+                    <h4>F.P : </h4>
+                </div>
+
+            </div>
         </div>
     )
 }
