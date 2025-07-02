68a4d7b63382440cbda52c5ee76bf4e8acb58ec1
Diogenes Borges <diogenes.borges@email.com>
2025-07-02 16:36:17 -0300

    Atualizacao da pagina Medidas

--- src/Paginas/Medidas.js
+++ src/Paginas/Medidas.js
@@ -1,20 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const ListaCards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit,minmax(300 px, 1fr));
+`;
+
+const ListaDevices = styled.div`
+    min-height: 30vg;
+    overflow: hidden;
+    padding: 5rem 10rem;
+
+`;
+
 
 const Medidas = ()=>{
     return (
-        <div>
-            <div className="card">
-                <image src = ''/>
-                <div>
-                    <h3>Device</h3>
-                    <h4>Kw/h :</h4>
-                    <h4>Corrente :</h4>
-                    <h4>Voltagem :</h4>
-                    <h4>F.P : </h4>
-                </div>
+        <ListaDevices>
+            <ListaCards>
+                <div className="card">
+                    <image src = ''/>
+                    <div>
+                        <h3>Device</h3>
+                        <h4>Kw/h :</h4>
+                        <h4>Corrente :</h4>
+                        <h4>Voltagem :</h4>
+                        <h4>F.P : </h4>
+                    </div>
 
-            </div>
-        </div>
+                </div>
+            </ListaCards>
+        </ListaDevices>
     )
 }
 
